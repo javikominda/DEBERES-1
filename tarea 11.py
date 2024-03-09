55be66b88585f7a9a6d0521795f4bd174c4e17ec
javikominda <javikoeljeff5@gmail.com>
2024-03-09 16:06:33 -0500

    Actualizacion

---
+++ tarea 11.py
@@ -0,0 +1,19 @@
+# Definir una matriz 3x3 con valores numéricos
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Imprimir la matriz original
+print("Matriz original:")
+for fila in matriz:
+    print(fila)
+
+# Modificar un valor en la matriz (por ejemplo, cambiar el valor en la posición [1][1] a 10)
+matriz[1][1] = 10
+
+# Imprimir la matriz modificada
+print("\nMatriz modificada:")
+for fila in matriz:
+    print(fila)
